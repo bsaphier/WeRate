f8d71cb62efd3320757acf1683c4597a36b248d3
Benjamin Saphier <b.saphier@gmail.com>
2018-01-26 13:34:29 -0500

    cleaned up App reducer

--- src/state/App/reducer.js
+++ src/state/App/reducer.js
@@ -4,44 +4,14 @@ import type { Action, rootState } from './types';
 
 
 
-const INIT_STATE: rootState = {
-  root: undefined
-  // placeFilter: {
-  //   filterType: '',
-  //   filterItems: [], // Tag ids
-  //   order: FILTER_ALPH_DESCENDING,
-  //   visibility: FILTER_PLACES_SHOW_ALL
-  // }
-};
-
+const INIT_STATE: rootState = { root: undefined };
 
 export default function (state: rootState = INIT_STATE, action: Action): rootState {
   switch (action.type) {
-
     case ROOT_CHANGED:
       return {
         root: action.root,
-        // placeFilter: { ...state.placeFilter }
       };
-
-    // case SET_PLACE_FILTER:
-    //   return {
-    //     root: state.root,
-    //     placeFilter: {
-    //       ...state.placeFilter,
-    //       ...action.payload
-    //     }
-    //   };
-
-    // case SET_PLACE_FILTER_ORDER:
-    //   return {
-    //     root: state.root,
-    //     placeFilter: {
-    //       ...state.placeFilter,
-    //       order: action.payload
-    //     }
-    //   };
-
     default:
       (action: empty);
       return state;
