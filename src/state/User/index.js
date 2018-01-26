ab28b90ac037d2be1874d0cf5731b1f2b7a9c60f
Benjamin Saphier <b.saphier@gmail.com>
2018-01-25 22:13:51 -0500

    deleted unused User reducer

--- src/state/User/index.js
+++
@@ -1,6 +0,0 @@
-import userReducer from './reducer';
-
-export { default as userTypes } from './types';
-export { default as actionCreators } from './action-creators';
-
-export default userReducer;
