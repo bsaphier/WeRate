a064ca5e3e6d7713c0f77c73ea7c743d820f46f5
Benjamin Saphier <b.saphier@gmail.com>
2018-02-12 19:12:43 -0500

    moved private key to local only file

---
+++ firebase-functions/env.js
@@ -0,0 +1,19 @@
+import { __MAILGUN_API_KEY__ } from './private';
+
+export const MAILGUN_BASE_URL = 'https://us-central1-werate-68084.cloudfunctions.net';
+
+export const MAILGUN_API_KEY = __MAILGUN_API_KEY__;
+
+export const FIRESTORE = {
+  USERS: 'users',
+  TAGS: 'tags',
+  PLACES: 'places',
+  REVIEWS: 'reviews',
+  REQ_ACCOUNT: '__users__'
+};
+
+export default {
+  FIRESTORE,
+  MAILGUN_API_KEY,
+  MAILGUN_BASE_URL
+};
