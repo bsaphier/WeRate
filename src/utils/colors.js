ef4b3e53f8b53c6e2b2f12c6f62754c5e8fae744
Benjamin Saphier <b.saphier@gmail.com>
2018-03-21 16:11:26 -0400

    color constants for styling

---
+++ src/utils/colors.js
@@ -0,0 +1,22 @@
+export default {
+  GREEN_MAIN: {
+    DARK: '#1D8C3A',
+    BASE: '#3F5F69',
+    LIGHT: '#90FFAD'
+  },
+  GREEN_DARK: {
+    DARK: '#005438',
+    BASE: '#009966',
+    LIGHT: '#73C7AB'
+  },
+  BLUE: {
+    DARK: '#26708C',
+    BASE: '#44CCFF',
+    LIGHT: '#99E3FF'
+  },
+  SHADE: {
+    DARK: '#282827',
+    BASE: '#494947',
+    LIGHT: '#9B9B9A'
+  }
+};
