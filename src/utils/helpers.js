c4c3dc31c2a8ad2c7ea523308f5f7fb4c617102a
Benjamin Saphier <b.saphier@gmail.com>
2018-04-03 15:30:14 -0400

    added helpers file

---
+++ src/utils/helpers.js
@@ -0,0 +1,16 @@
+// @flow
+
+/**
+ * Creates an object from the keys passed in. Each key's value will be defaultValue.
+ * If nothing is passed as defaultValue, then all the values will be set to false
+ * 
+ * @param {Array<string>} keys 
+ * @param {*} defaultValue 
+ */
+export const objFromKeys = (keys: string[], defaultValue: boolean | any = false) => {
+  const returnObj = {};
+  keys.forEach(keyName => {
+    returnObj[keyName] = defaultValue;
+  });
+  return returnObj;
+};
